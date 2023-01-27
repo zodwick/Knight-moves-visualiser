c1be25a2989f79d30b45fa9768616a85942ebf3b
zodwick <115868723+zodwick@users.noreply.github.com>
2023-01-27 14:40:45 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Board from './chess'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Board></Board>
+ </React.StrictMode>,
+)
